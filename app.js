fb8c2ac547659ed0146e3c5d12ebeef37f672f7c
RahmadAmri <rahmadamri3@gmail.com>
2025-05-28 21:07:07 +0700

    Feat: done crud

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
-require("dotenv").config();
 const app = express();
 const PORT = 3000;
-const router = require("./routers");
+const Controller = require("./controllers/controller");
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", Controller.getProgressLog);
+app.get("/user-preferences/:UserId", Controller.getUserPreferences);
+app.put("/user-preferences/:UserId", Controller.updateUserPreferences);
+app.post("/progressLog", Controller.createProgressLog);
+app.delete("/progressLog/:id", Controller.deleteProgressLog);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}`);
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,18 +1,25 @@
-const { ProgressLog, UserPreference } = require("../models");
+const { ProgressLog, UserPreference, User } = require("../models");
 
 class Controller {
-  static async getProgressLog(req, res) {
+  static async getProgressLog(req, res, next) {
     try {
       const data = await ProgressLog.findAll({
-        attributes: ["caloriesBurned", "fitnessGoal"],
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        include: [
+          {
+            model: User,
+            attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+          },
+        ],
       });
+
       res.status(200).json(data);
     } catch (error) {
       next(error);
     }
   }
 
-  static async getUserPreferences(req, res) {
+  static async getUserPreferences(req, res, next) {
     try {
       const { UserId } = req.params;
       const preferences = await UserPreference.findAll({
@@ -24,23 +31,74 @@ class Controller {
       next(error);
     }
   }
-  static async updateUserPreferences(req, res) {
+  static async updateUserPreferences(req, res, next) {
     try {
       const { UserId } = req.params;
-      const { sportType, intensityLevel } = req.body;
+      const { preferredSports, fitnessGoal } = req.body;
 
       const [updated] = await UserPreference.update(
-        { sportType, intensityLevel },
-        { where: { userId } }
+        { preferredSports, fitnessGoal },
+        { where: { UserId } }
       );
 
       if (updated) {
         const updatedPreference = await UserPreference.findOne({
-          where: { userId },
+          where: { UserId },
         });
-        res.status(200).json(updatedPreference);
+        res.status(201).json({
+          message: `User preferences ${preferredSports} updated successfully`,
+          updatedPreference,
+        });
       } else {
         res.status(404).json({ message: "User preferences not found" });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async createProgressLog(req, res, next) {
+    try {
+      const { sport, duration, caloriesBurned, UserId } = req.body;
+      const newLog = await ProgressLog.create({
+        sport,
+        duration: +duration,
+        caloriesBurned: +caloriesBurned,
+        UserId,
+      });
+
+      const logWithUser = await ProgressLog.findByPk(newLog.id, {
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        include: [
+          {
+            model: User,
+            attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+          },
+        ],
+      });
+
+      res.status(201).json({
+        message: `Progress log with name ${sport} successfully added`,
+        data: logWithUser,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async deleteProgressLog(req, res, next) {
+    try {
+      const { id } = req.params;
+      const findLog = await ProgressLog.findByPk(id, {
+        attributes: ["sport"],
+      });
+      const deleted = await ProgressLog.destroy({ where: { id } });
+      if (deleted) {
+        res.status(200).json({
+          message: `Progress log deleted with sport ${
+            findLog ? findLog.sport : "unknown"
+          } successfully`,
+        });
+      } else {
+        res.status(404).json({ message: "Progress log not found" });
       }
     } catch (error) {
       next(error);
